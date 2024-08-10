0c0ebde46a6914d755f171a4da93358a633d8160
Raphael Busquet <73319921+raphaelbusquet@users.noreply.github.com>
2024-08-10 22:16:03 +0100

    inicial commit

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -16,15 +16,16 @@ namespace DesafioProjetoHospedagem.Models
         public void CadastrarHospedes(List<Pessoa> hospedes)
         {
             // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
-            // *IMPLEMENTE AQUI*
-            if (true)
+            bool verificarCapacidade = Suite.Capacidade >= hospedes.Count;
+
+            if (verificarCapacidade)
             {
                 Hospedes = hospedes;
             }
             else
             {
                 // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
-                // *IMPLEMENTE AQUI*
+                throw new Exception("Número de hóspedes ultrapassa o limite da suíte escolhida.");
             }
         }
 
@@ -36,7 +37,7 @@ namespace DesafioProjetoHospedagem.Models
         public int ObterQuantidadeHospedes()
         {
             // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
-            // *IMPLEMENTE AQUI*
+            int quantidadeHospedes = Hospedes.Count;
             return 0;
         }
 
@@ -44,14 +45,12 @@ namespace DesafioProjetoHospedagem.Models
         {
             // TODO: Retorna o valor da diária
             // Cálculo: DiasReservados X Suite.ValorDiaria
-            // *IMPLEMENTE AQUI*
-            decimal valor = 0;
+            decimal valor = DiasReservados * Suite.ValorDiaria;
 
             // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
-            // *IMPLEMENTE AQUI*
-            if (true)
+            if (DiasReservados >= 10)
             {
-                valor = 0;
+                valor -= valor*10/100;
             }
 
             return valor;
